7d8648da563460ec11b4ad3e0d4b05ac8092be86
Jacob <jmatsumoto@students.mchenry.edu>
2025-04-03 22:06:46 -0500

    some work

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,49 @@
 /* https://www.w3schools.com/js/tryit.asp?filename=tryjs_intro_lightbulb */
 
-
+const items = [
+    {
+        picture: "images/duckstar.png", 
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet repellat praesentium eius expedita cupiditate veniam quibusdam harum non dicta distinctio suscipit ducimus consequatur, nobis nam excepturi reiciendis architecto magnam officiis?", 
+        title:"this one is the duckstar"
+    },
+    {
+        picture: "images/duckvader.png", 
+        paragraph: "duckvader Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet repellat praesentium eius expedita cupiditate veniam quibusdam harum non dicta distinctio suscipit ducimus consequatur, nobis nam excepturi reiciendis architecto magnam officiis?", 
+        title: "this is duckvader"
+    },
+    {
+        picture: "images/duckbacca.png",
+        paragraph:"lorem",
+        title:"duckbaca",
+    },
+    {
+        picture: "images/leia.png",
+        paragraph:"lorem2",
+        title:"leia"
+    },
+    {
+        picture:"images/wookieandrobot.png",
+        paragraph:"lorem3",
+        title:"wookie and robot"
+    }
+]
+function change_on_click(index) {
+    const item = items[index];
+    change(item.picture, item.paragraph, item.title);
+}
 
 /* change the image */
-function change(picture) {
+function change(picture, paragraph, title) {
     document.getElementById("large").src = picture; 
+    document.getElementById("paragraph_display").innerHTML = paragraph;
+    document.getElementById("title_display").innerHTML = title;
+
+}
+
+let index_change = 0
+
+change();{
+    let item = items[index_change];
+    change(item.picture,item.paragraph,item.title)
+    index_change = (index_change + 1) % items.length;
 }
